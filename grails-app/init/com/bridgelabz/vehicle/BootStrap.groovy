88ed545ea610f7a3a5201161b3e8212f435cd5bb
aviii007 <kanojia.avi@gmail.com>
2023-02-08 00:01:57 +0530

    Added:The make and model in bootstrap

--- grails-app/init/com/bridgelabz/vehicle/BootStrap.groovy
+++ grails-app/init/com/bridgelabz/vehicle/BootStrap.groovy
@@ -1,8 +1,29 @@
 package com.bridgelabz.vehicle
 
+import groovy.transform.CompileStatic
+
+@CompileStatic
 class BootStrap {
 
+    MakeService makeService
+    ModelService modelService
+    VehicleService vehicleService
+
+        
+    
     def init = { servletContext ->
+    Make tata = makeService.save( 'Tata')
+        Make honda = makeService.save('Honda')
+        Make kia = makeService.save('Kia')
+
+        Model winger = modelService.save('Winger', tata)
+        Model amaze = modelService.save('Amaze', honda)
+        Model carnival = modelService.save('Carnival', kia)
+
+        vehicleService.save('Pickup', tata, winger, 2012).save()
+        vehicleService.save('Economy', honda, amaze, 2014).save()
+        vehicleService.save('Minivan', kia, carnival, 1990).save()
+    
     }
     def destroy = {
     }
