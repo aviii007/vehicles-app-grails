777fee2ce3aec8377228913e281a0041cc1138e5
aviii007 <kanojia.avi@gmail.com>
2023-02-07 23:51:47 +0530

    Refactor: The vehicle class to use make and model instead of String

--- grails-app/domain/com/bridgelabz/vehicle/Vehicle.groovy
+++ grails-app/domain/com/bridgelabz/vehicle/Vehicle.groovy
@@ -2,6 +2,14 @@ package com.bridgelabz.vehicle
 
 class Vehicle {
 
+    String name 
+    Model model
+    Make make
+
     static constraints = {
+
+        year min: 1900
+        name maxSize: 255
+        
     }
 }
